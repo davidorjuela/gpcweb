e7aeb7e9427eb8391b1a271207d64f787244f744
davidorjuela <davidorjuel@gmail.com>
2021-08-06 22:04:36 -0500

    6th commit

---
+++ index.js
@@ -0,0 +1,15 @@
+window.onload = function (){
+    let logo = document.getElementById("logo");
+    window.onscroll = function() {
+        if (window.scrollY>110){
+        logo.style.position= 'fixed';
+        logo.style.top= '-0.7rem';
+        logo.style.left= '-0.7rem';
+        }
+        else if(window.scrollY<10){
+            logo.style.position= 'relative';
+            logo.style.top= '0';
+            logo.style.left= '-0';
+        }
+    }
+}
